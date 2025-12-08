4c1bb86c4012b648cc6c54289983c803058632d5
Heather <224328814+HeatherR206@users.noreply.github.com>
2025-12-08 15:04:36 -0800

    feat(components): update app components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,113 +1,55 @@
 import { useState } from "react";
-import { MovieCard } from "../main-card/movie-card";
+import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Pride & Prejudice",
-            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FsGjIvtVvTlWnia2zfJfHz81pZ9Q.jpg&f=1&nofb=1&ipt=af242cdb32fd25ef8b9f962bc2d95f06542d46916bb3c24b908bc34313cebebd",
-            director: ([
-                {
-                    directorName:"Joe Wright"
-                },
-            ]),
-            cast: ([
-                {
-                    actorName: "Keira Knightley",
-                },
-                {
-                    actorName: "Matthew Macfadyen",
-                },
-                {
-                    actorName: "Donald Sutherland",
-                },
-                {
-                    actorName: "Brenda Blethyn"
-                }
-            ]),
-            releaseDate: "2005-09-16"
-        },
-        {
-            id: 2,
-            title: "The Scent of Green Papaya",
-            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FM%2FMV5BYmQ1NzFhNDYtNThhMi00YjljLWFhM2QtMWMzYTI5NTVkMGQ2XkEyXkFqcGdeQXVyNjMwMjk0MTQ%40._V1_.jpg&f=1&nofb=1&ipt=e366e8c1bfb5e63c51eb4f44eb144598f40f4d87f4cb2f67532f566070d7d139",
-            director: ([
-                {
-                    directorName: "Trần Anh Hùng"
-                },
-            ]),
-            cast: ([
-                {
-                    actorName: "Tran Nu Yên-Khê",
-                },
-                {
-                    actorName: "Man San Lu",
-                },
-                {
-                    actorName: "Thi Loc Truong"
-                }
-            ]),
-            releaseDate: "1993-12-08"
-        },
-        {
-            id: 3,
-            title: "A Room with a View",
-            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FM%2FMV5BYTEyYmE0M2MtZDQ1OC00MGUxLWI4OGMtM2M1ZjIyNDI0Y2Q4XkEyXkFqcGdeQXVyNjQ2MjQ5NzM%40._V1_.jpg&f=1&nofb=1&ipt=d29be2b413de8c8284f66d943c92c54e1b218529898af6454f3eea6ed2133a0c",
-            director: ([
-                {
-                    directorName: "James Ivory"
-                },
-            ]),
-            cast: ([
-                {
-                    actorName: "Maggie Smith",
-                },
-                {
-                    actorName: "Helena Bonham Carter",
-                },
-                {
-                    actorName: "Denholm Elliott",
-                },
-                {
-                    actorName: "Julian Sands"
-                }
-            ]),
-            releaseDate: "1986-12-19"
-        },
-        {
-            id: 4,
-            title: "Stand by Me",
-            image: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2Fvz0w9BSehcqjDcJOjRaCk7fgJe7.jpg&f=1&ipt=6c111f776302588eeac5adf65db61eb6da8569ed1d30e44d9454a77ae5728767",
-            director: ([
-                {
-                    directorName: "Rob Reiner"
-                },
-            ]),
-            cast: ([
-                {
-                    actorName: "Wil Wheaton",
-                },
-                {
-                    actorName: "River Phoenix",
-                },
-                {
-                    actorName: "Jerry O'Connell",
-                },
-                {
-                    actorName: "Corey Feldman"
-                }
-            ]),
-            releaseDate: "1986-08-08"
-        }
+      {
+        id: 1,
+        title: "Pride & Prejudice",
+        image: "https://m.media-amazon.com/images/I/71GspoOXkeL._SL1500_.jpg",
+        summary:
+          "Five sisters in an English family deal with issues of marriage, morality, and misconceptions when two wealthy bachelors arrive in their neighborhood.",
+        genre: "Romance",
+        director: "Joe Wright",
+      },
+      {
+        id: 2,
+        title: "The Scent of Green Papaya",
+        image: "https://m.media-amazon.com/images/I/71t6r3Y3WtL._SL1077_.jpg",
+        summary:
+          "In 1950s Saigon, a young servant girl, Mui, begins work for a wealthy but troubled Vietnamese family, finding beauty in her attentive daily routines.",
+        genre: "Drama",
+        director: "Trần Anh Hùng",
+      },
+      {
+        id: 3,
+        title: "A Room with a View",
+        image: "https://m.media-amazon.com/images/I/819maqMLyOL._SL1500_.jpg",
+        summary:
+          "A young Englishwoman, Lucy, on a tour of Italy must choose between a charming, free-spirited man she meets there and a wealthy, priggish suitor back home.",
+        genre: "Romantic Comedy, Drama",
+        director: "James Ivory",
+      },
+      {
+        id: 4,
+        title: "Stand by Me",
+        image: "https://m.media-amazon.com/images/I/81PmlArGyfS._SL1500_.jpg",
+        summary:
+          "Four young friends in a small Oregon town go on an unforgettable, two-day search for the body of a missing boy, leading to a journey of self-discovery.",
+        genre: "Adventure, Humor, Family",
+        director: "Rob Reiner",
+      },
     ]);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
     if (selectedMovie) {
         return (
-            <MovieView movie={ selectedMovie } onBackClick={ () => setSelectedMovie(null) } />
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={() => setSelectedMovie(null)}
+          />
         );
     }
 
@@ -116,16 +58,16 @@ export const MainView = () => {
     }
 
     return (
-        <div>
-            {movies.map((movie) => (
-                <MovieCard
-                    key={ movie.id }
-                    movie={ movie }
-                    onMovieClick={ (newSelectedMovie) => {
-                        setSelectedMovie(newSelectedMovie);
-                    }}
-                />
-            ))}
-        </div>
+      <div>
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ))}
+      </div>
     );
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,26 @@
 export const MovieView = ({ movie, onBackClick }) => {
     return (
+      <div>
         <div>
-            <div>
-                <h2>Title: </h2>
-                <h2>{ movie.title }</h2>
-            </div>
-            <div>
-                <img src={ movie.image } alt={ movie.title } />
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{ movie.director.directorName }</span>
-            </div>
-            <div> 
-                <span>Cast: </span>
-                <span>[{ movie.cast.actorName }]</span>
-            </div>
-            <div>
-                <span>Release Date: </span>
-                <span>{ movie.releaseDate }</span>
-            </div>
-            <button onClick={ onBackClick }>Back to movie list</button>
+          <span>Title: </span>
+          <span>{movie.title}</span>
         </div>
+        <div>
+          <img src={movie.image} />
+        </div>
+        <div>
+          <span>Summary: </span>
+          <span>{movie.summary}</span>
+        </div>
+        <div>
+          <span>Director: </span>
+          <span>{movie.director}</span>
+        </div>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre}</span>
+        </div>
+        <button onClick={onBackClick}>Back</button>
+      </div>
     );
 };
